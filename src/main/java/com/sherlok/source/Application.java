ff88558cef4ea86b161cabb7c6742d1205f47856
sherlok6 <skibakol@yandex.ru>
2021-11-13 22:13:57 +0300

    Completed?

--- src/main/java/com/sherlok/source/Application.java
+++ src/main/java/com/sherlok/source/Application.java
@@ -1,7 +1,22 @@
 package com.sherlok.source;
 
-public class Application {
-    public static void main(String args[]){
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
 
+public class Application {
+
+    public static void main(String args[]) throws MalformedURLException {
+        Scanner scanner = new Scanner(System.in);
+        AnalyzerText analyzerText = new AnalyzerText();
+
+        System.out.println("Укажите URL:");
+        URL url = new URL(scanner.next());
+
+        analyzerText.initDict(url.toString());
+
+        Dictionary dictionary = Dictionary.getInstance();
+
+        System.out.println(dictionary.toString());
     }
 }
