5fd845810edad5bdb2a0beaf8c8f026f089bfbf9
sherlok6 <skibakol@yandex.ru>
2021-11-14 21:48:20 +0300

    Add javadoc for App

--- src/main/java/com/sherlok/source/Application.java
+++ src/main/java/com/sherlok/source/Application.java
@@ -6,7 +6,16 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
+/**Класс с которого приложение начинает работу содержит <b>log</b>{@link Logger} и метод для запуска.
+ * Программа считывает с консоли url адрес страницы, обрабатывает её с помощью объекта класса {@link AnalyzerText}
+ * и выводит результат анализа, хранящийся в объекте-одиночке{@link Dictionary}.
+ *
+ * @author sherlok6
+ * @version 1.0
+ * **/
 public class Application {
+
+    /** Объект для логирования, полученный методом {@link Logger#getLogger(Class)}**/
     private static final Logger log = Logger.getLogger(Application.class);
 
     public static void main(String args[]) throws MalformedURLException {
@@ -16,7 +25,7 @@ public class Application {
         System.out.println("Укажите URL:");
         URL url = new URL(scanner.next());
 
-        analyzerText.initDict(url.toString());
+        analyzerText.start(url.toString());
 
         log.info("Был введен следующий url:" + url.toString());
 
