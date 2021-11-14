2f3bd291498a3f4f0d5e36610433ad5b76f881bf
sherlok6 <skibakol@yandex.ru>
2021-11-14 21:40:21 +0300

    Add javadoc AnalyzerText

--- src/main/java/com/sherlok/source/AnalyzerText.java
+++ src/main/java/com/sherlok/source/AnalyzerText.java
@@ -9,35 +9,55 @@ import com.sherlok.source.url.SourceURL;
 import com.sherlok.source.url.impl.SourceURLImpl;
 import org.apache.log4j.Logger;
 
-import java.net.MalformedURLException;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.stream.Stream;
 
+/**Класс, анализирующий получаемый из <b>sourceURL</b>{@link SourceURL},
+ *   обработанный с помощью <b>processingString</b>{@link ProcessingString}
+ *   и <b>processingSource</b>{@link ProcessingSource} и хранимый в <b>text</b>.
+ * Содержит ссылку на "одиночку"<b>dictionary</b>{@link Dictionary} и объект для логирования <b>log</b>
+ *
+ * @author sherlok6(Skiba Nikolai)
+ * @version 1.0
+ * **/
 public class AnalyzerText {
 
+    /** Объект для логирования, полученный методом {@link Logger#getLogger(Class)}**/
     private static final Logger log = Logger.getLogger(AnalyzerText.class);
 
+    /**Ссылка на одиночку**/
     Dictionary dictionary;
 
+    /**Для обработки URL-запроса**/
     private SourceURL sourceURL = new SourceURLImpl();
 
-    String text;
+    /**Свойство для текста страницы**/
+    private String text;
 
+    /**Для обработки текста,замена разделителей**/
     private ProcessingString processingString = new ProcessingStringImpl();
+
+    /**Для разбиения на слова**/
     private ProcessingSource processingSource = new ProcessingSourceImpl();
 
+    /**Констуктор для создания нового объекта**/
     public AnalyzerText(){};
 
-    public void initDict(String path) throws MalformedURLException {
+    /**Запускает анализ по указанному пути {@param path}. Делает запись в лог-файле о получении текста**/
+    public void start(String path){
             text = sourceURL.getBuffHtml(path);
             log.info("Получили текст страницы");
             text = processingString.replaceSigns(text);
-            toDict(processingSource.split(text));
+            fillDict(processingSource.split(text));
     }
 
-    private void toDict(String[] words){
+    /**Метод заполняет словарь словами из массива слов{@param words}. Для определения кол-ва дубликтов
+     * использовался {@link Collections#frequency(Collection, Object)} в качестве коллекции использовался
+     * объект класса {@link LinkedList}. Делает запись в лог о количестве слов в тексте**/
+    private void fillDict(String[] words){
         int count = words.length;
 
         dictionary = Dictionary.getInstance();
@@ -52,7 +72,6 @@ public class AnalyzerText {
             Word w = new Word(str, Collections.frequency(wordsList, str));
             if(!dictionary.containWord(w)) {
                 dictionary.addWord(w);
-                dictionary.incCount(w.getCount());
             }
         });
     }
