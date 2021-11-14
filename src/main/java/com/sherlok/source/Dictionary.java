df8a8d24a7c4f40387570411417ec675d69923b6
sherlok6 <skibakol@yandex.ru>
2021-11-14 21:12:38 +0300

    Add javadoc for Dictionary

--- src/main/java/com/sherlok/source/Dictionary.java
+++ src/main/java/com/sherlok/source/Dictionary.java
@@ -6,20 +6,37 @@ import org.apache.log4j.Logger;
 import java.util.Set;
 import java.util.TreeSet;
 
+/**Ялвяется lazy-реализацией Singleton, содержит объект для логирования, свойство для хранения объекта-одиночки,
+ * коллекцию <b>wordSet</b>, и свойство <b>wordCount</b>.
+ * Хранит информацию о тексте: о общем количестве слов в тексте и инфформацию о каждом слове
+ * {@link Word}, хранящихся в <b>wordSet</b>
+ *
+ * @author sherlok6
+ * @version 1.0
+ * **/
 public class Dictionary {
 
+    /** Объект для логирования, полученный методом {@link Logger#getLogger(Class)}**/
     private static final Logger log = Logger.getLogger(Dictionary.class);
 
+    /**Свойство для хранения объекта-одиночки**/
     private static Dictionary instance;
 
-    private Set<Word> wordSet;
+    /**{@link TreeSet} для хранения объектов класса {@link Word}**/
+    private TreeSet<Word> wordSet;
 
+    /**Свойство - общее количество слов**/
     private int wordCount;
 
+    /**Конструктор для создания одиночного объекта и инициализации пустого wordSet**/
     private Dictionary(){
-        this.wordSet = new TreeSet<>();
+        wordSet = new TreeSet<>();
     }
 
+    /**Метод для извлечения уже созданного объекта,если создан, иначе создаёт,делает запись в лог-файл,
+     * такой и возвращает его
+     * @return instance
+     **/
     public static Dictionary getInstance(){
         if (instance == null){
             instance = new Dictionary();
@@ -28,26 +45,31 @@ public class Dictionary {
         return instance;
     }
 
+    /** Задает количество слов в тексте, которое можно получить при помощи метода {@link #getWordCount()}
+     @param wCount Новое значение wordCount*/
     public void setWordCount(int wCount){
         this.wordCount = wCount;
     }
 
-    public void incCount(int diff){
-        wordCount -= diff;
-    }
-
+    /**Метод для добавления нового слова в wordSet**/
     public void addWord(Word w){
         this.wordSet.add(w);
     }
 
+    /**Проверяет находится ли {@param w} в wordSet
+     * @return false-нет,true-да
+     **/
     public boolean containWord(Word w){
         return wordSet.contains(w);
     }
 
+    /**Получает значение свойства wordCount, которое можно задать с помощью метода {@link #setWordCount(int)}}
+     @return Значение свойства wordCount**/
     public int getWordCount(){
         return wordCount;
     }
 
+    /**Выводит всю информацию о найденных словах,если wordSet не пуст**/
     @Override
     public String toString() {
         if(wordSet!=null){
